4ecee703c9dc37655d0e9719f7734594031144de
estefaniaderosa <estefania.derosa@gmail.com>
2021-04-01 01:47:48 +0200

    bg change img for video

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ import './Hero.css'
 export const Hero = () => {
     return (
         <div className="hero-container">
-            <video src='https://github.com/estefaniaderosa/gbhotel/blob/gh-pages/video/beach2.mp4?raw=true' autoPlay muted loop/>
+            {/* <video src='https://github.com/estefaniaderosa/gbhotel/blob/gh-pages/video/beach2.mp4?raw=true' autoPlay muted loop/> */}
             <h1>Grand Beach Hotel</h1>
             <p>What are you waiting for?</p>
             <div className="hero-btns">
